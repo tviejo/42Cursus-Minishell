f406bc6e321caa2819166c2fdf9f332429ad299f
mig29gm@gmail.com <mig29gm@gmail.com>
2024-07-18 16:57:06 +0200

    ades-1807-1657

--- includes/mini_ade_sarr.h
+++ includes/mini_ade_sarr.h
@@ -6,14 +6,14 @@
 /*   By: ade-sarr <ade-sarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 17:17:03 by ade-sarr          #+#    #+#             */
-/*   Updated: 2024/07/17 23:50:51 by ade-sarr         ###   ########.fr       */
+/*   Updated: 2024/07/18 16:44:32 by ade-sarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINI_ADE_SARR_H
 # define MINI_ADE_SARR_H
 
-# include "../libft_ades/libft_ades.h"
+# include "../libft/libft.h"
 # include <stdlib.h>
 # include <stdio.h>
 # include <readline/readline.h>
